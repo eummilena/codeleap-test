1fbd9f0881004bc348fdfc0e533a59716987401e
Milena Mota <80923819+eummilena@users.noreply.github.com>
2026-03-16 12:51:14 -0300

    feat - componente button

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+type ButtonProps = React.ComponentProps<'button'>
+
+const Button = ({ className, children, ...props }: ButtonProps) => {
+    return (
+
+        <button className={className} {...props}>{children}</button>
+    )
+}
+
+export default Button
